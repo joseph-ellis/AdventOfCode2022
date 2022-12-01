76fada4787fc3f976110dbb966771ea0da38c98f
Joe Ellis <282555@carmax.com>
2022-12-01 10:38:39 -0500

    Remove reference to tidyverse and explicitly loaded individual packages. Also added namesapce to each function call not inlcuded in base R.

--- Day 1/Puzzle 1/aoc_1.R
+++ Day 1/Puzzle 1/aoc_1.R
@@ -1,9 +1,13 @@
 
 # ADVENT OF CODE - DAY 1
 
-library(tidyverse)
+library(readr)
+library(purrr)
+library(magrittr)
+library(tibble)
+library(dplyr)
 
-input <- read_csv(
+input <- readr::read_csv(
   file = "input.csv",
   skip_empty_rows = FALSE
 )
@@ -13,26 +17,34 @@ input <- read_csv(
 # FUNCTION - splits a vector into a list on each NA entry
 split_vec_na <- function(.vector) {
   is_sep <- is.na(.vector)
-  split(.vector[!is_sep], cumsum(is_sep)[!is_sep])
+  split(
+    x = .vector[!is_sep],
+    f = cumsum(is_sep)[!is_sep])
 }
 
 # split the input into a list using the function defined above
-elf_list <- split_vec_na(input)
+elf_list <- split_vec_na(.vector = input)
 
 # generate a new list with the total calories for each elf
-elf_totals <- map(
+elf_totals <- purrr::map(
   .x = elf_list,
   .f = sum
 )
 
 # get the maximum number of calories in the list
-elf_max <- max(unlist(elf_totals))
+elf_max <- max(
+  unlist(x = elf_totals)
+)
 
 
 # PART 2 - find the top three elves with the most calories
 elf_sorted <- elf_totals %>%
   unlist() %>%
-  as_tibble %>%
-  arrange(desc(value)) %>%
-  mutate(csum = cumsum(value)) %>%
-  slice_head(n = 3)
+  tibble::as_tibble() %>%
+  dplyr::arrange(
+    dplyr::desc(x = value)
+  ) %>%
+  dplyr::mutate(
+    csum = cumsum(x = value)
+  ) %>%
+  dplyr::slice_head(n = 3)
